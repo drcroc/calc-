59eef25a1c95fcb3b2dfac5f922606b3847403e3
drcroc <kuroui2000@gmail.com>
2020-04-11 17:35:02 +0300

    not using namespace stdl;

--- Source.cpp
+++ Source.cpp
@@ -7,7 +7,7 @@
 
 # define M_PI           3.14159265358979323846  /* pi */
 #define _USE_MATH_DEFINES
-using namespace std;
+//using namespace std;
 
 
 int c;
@@ -19,13 +19,13 @@ class MDSSrASub {
 public :
 	double x,y,z;
 
-	int add(int, int) {system("CLS");	cout << "You have choosen addition ...." << endl;	cout << "Enter x :=: ";cin >> x;	cout << "Enter y :=: ";cin >> y;	z = x + y;	return z; }
+	int add(int, int) {system("CLS");	std::cout << "You have choosen addition ...." <<std::endl;	std::cout << "Enter x :=: ";std::cin >> x;	std::cout << "Enter y :=: ";std::cin >> y;	z = x + y;	return z; }
 
-	int mult(int , int) {system("CLS");	cout << "You have choosen multiplying ...." << endl;	cout << "Enter x :=: ";cin >> x;	cout << "Enter y :=: ";cin >> y;	z = x * y;	return z;}
+	int mult(int , int) {system("CLS");	std::cout << "You have choosen multiplying ...." <<std::endl;	std::cout << "Enter x :=: ";std::cin >> x;	std::cout << "Enter y :=: ";std::cin >> y;	z = x * y;	return z;}
 
-	int div(float, float) {system("CLS");	cout << "You have choosen dividing ...." << endl;	cout << "Enter x :=: ";cin >> x;	cout << "Enter y :=: ";cin >> y;	 z = x / y; return z;}
+	int div(float, float) {system("CLS");	std::cout << "You have choosen dividing ...." <<std::endl;	std::cout << "Enter x :=: ";std::cin >> x;	std::cout << "Enter y :=: ";std::cin >> y;	 z = x / y; return z;}
 
-	int sub(int, int) {system("CLS");	cout << "You have choosen subtraction ...." << endl;	cout << "Enter x :=: ";cin >> x;	cout << "Enter y :=: ";cin >> y;	 z = x - y; return z;}
+	int sub(int, int) {system("CLS");	std::cout << "You have choosen subtraction ...." <<std::endl;	std::cout << "Enter x :=: ";std::cin >> x;	std::cout << "Enter y :=: ";std::cin >> y;	 z = x - y; return z;}
 };
 MDSSrASub m;
 
@@ -36,13 +36,13 @@ public:
 	int h,b;
 	double x, y, z, r;
 	
-	int AoS(int) { system("CLS"); cout << "You have choosen Area of sqare ...." << endl;	cout << "Enter x :=: ";cin >> x;	z = x * x;	return z; }
+	int AoS(int) { system("CLS"); std::cout << "You have choosen Area of sqare ...." <<std::endl;	std::cout << "Enter x :=: ";std::cin >> x;	z = x * x;	return z; }
 
-	int AoR(int, int) { system("CLS"); cout << "You have choosen Area of rectangle  ...." << endl;	cout << "Enter x :=: ";cin >> x;	cout << "Enter y :=: ";cin >> y;	 z = x * y;	return z; }
+	int AoR(int, int) { system("CLS"); std::cout << "You have choosen Area of rectangle  ...." <<std::endl;	std::cout << "Enter x :=: ";std::cin >> x;	std::cout << "Enter y :=: ";std::cin >> y;	 z = x * y;	return z; }
 
-	int AoT(int, int) { system("CLS"); cout << "You have choosen Area of triangle ...." << endl;	cout << "Enter height in cm:=: ";cin >> h;	cout << "Enter base  in cm :=: ";cin >> b;	 z = (0.5) * h * b;	return z; }
+	int AoT(int, int) { system("CLS"); std::cout << "You have choosen Area of triangle ...." <<std::endl;	std::cout << "Enter height in cm:=: ";std::cin >> h;	std::cout << "Enter base  in cm :=: ";std::cin >> b;	 z = (0.5) * h * b;	return z; }
 
-	int AoC(int) { system("CLS"); cout << "You have choosen Area of circle ...." << endl;	cout << "Enter r :=: ";cin >> r;	z = r*r*M_PI;	return z; }
+	int AoC(int) { system("CLS"); std::cout << "You have choosen Area of circle ...." <<std::endl;	std::cout << "Enter r :=: ";std::cin >> r;	z = r*r*M_PI;	return z; }
 };
 AsArAtAc Am;
 
@@ -52,13 +52,13 @@ class PsPtPc {
 public:
 	double x, y, z,c,r;
 
-	int PoS(int) { system("CLS"); cout << "You have choosen Perimeter of sqare ...." << endl;	cout << "Enter x :=: ";cin >> x;	z = x*4;	return z; }
+	int PoS(int) { system("CLS"); std::cout << "You have choosen Perimeter of sqare ...." <<std::endl;	std::cout << "Enter x :=: ";std::cin >> x;	z = x*4;	return z; }
 
-	int PoR(int, int) { system("CLS"); cout << "You have choosen Perimeter of rectangle  ...." << endl;		cout << "Enter x :=: ";cin >> x;	cout << "Enter y :=: ";cin >> y;	z = x*2 + y*2;	return z; }
+	int PoR(int, int) { system("CLS"); std::cout << "You have choosen Perimeter of rectangle  ...." <<std::endl;		std::cout << "Enter x :=: ";std::cin >> x;	std::cout << "Enter y :=: ";std::cin >> y;	z = x*2 + y*2;	return z; }
 
-	int PoT(int, int, int) { system("CLS"); cout << "You have choosen Perimeter of triangle ...." << endl;	cout << "Enter side a:=: ";cin >> x;	cout << "Enter side b :=: ";cin >> y; cout << "Enter side c :=: ";cin >> c;	 z = x+y+c;	return z; }
+	int PoT(int, int, int) { system("CLS"); std::cout << "You have choosen Perimeter of triangle ...." <<std::endl;	std::cout << "Enter side a:=: ";std::cin >> x;	std::cout << "Enter side b :=: ";std::cin >> y; std::cout << "Enter side c :=: ";std::cin >> c;	 z = x+y+c;	return z; }
 
-	int PoC(int) { system("CLS"); cout << "You have choosen Perimeter of circle ...." << endl;	cout << "Enter r :=: ";cin >> r;	z = r * 2 * M_PI;	return z; }
+	int PoC(int) { system("CLS"); std::cout << "You have choosen Perimeter of circle ...." <<std::endl;	std::cout << "Enter r :=: ";std::cin >> r;	z = r * 2 * M_PI;	return z; }
 };
 PsPtPc Pm;
 
@@ -67,13 +67,13 @@ class VcVpVs {
 public:
 	double x, y, z,h;
 
-	int VoC(int) { system("CLS"); cout << "You have choosen Volume of cube ...." << endl;	cout << "Enter x :=: ";cin >> x;	z = x * x * x;	return z; }
+	int VoC(int) { system("CLS"); std::cout << "You have choosen Volume of cube ...." <<std::endl;	std::cout << "Enter x :=: ";std::cin >> x;	z = x * x * x;	return z; }
 
-	int VoP(int, int, int) { system("CLS"); cout << "You have choosen Volume of pyramid ...." << endl;	cout << "Enter Base length :=: ";	cin >> x;	cout << "Enter Base width :=: ";	cin >> y;	cout << "Enter Pyramid height :=: ";	cin >> h;	z = (x*y*h)/3;	return z; }
+	int VoP(int, int, int) { system("CLS"); std::cout << "You have choosen Volume of pyramid ...." <<std::endl;	std::cout << "Enter Base length :=: ";	std::cin >> x;	std::cout << "Enter Base width :=: ";	std::cin >> y;	std::cout << "Enter Pyramid height :=: ";	std::cin >> h;	z = (x*y*h)/3;	return z; }
 
-	int VoS(int) { system("CLS"); cout << "You have choosen Volume of sphere ...." << endl;	cout << "Enter x :=: ";cin >> x;	z=(4/3)*M_PI*x*x*x;	return z; }
+	int VoS(int) { system("CLS"); std::cout << "You have choosen Volume of sphere ...." <<std::endl;	std::cout << "Enter x :=: ";std::cin >> x;	z=(4/3)*M_PI*x*x*x;	return z; }
 
-	int VoR(int, int, int) { system("CLS"); cout << "You have choosen Volume of rectangle ...." << endl;	cout << "Enter Base length :=: ";	cin >> x;	cout << "Enter Base width :=: ";	cin >> y;	cout << "Enter Pyramid height :=: ";	cin >> h;	z = (x * y * h);	return z;}
+	int VoR(int, int, int) { system("CLS"); std::cout << "You have choosen Volume of rectangle ...." <<std::endl;	std::cout << "Enter Base length :=: ";	std::cin >> x;	std::cout << "Enter Base width :=: ";	std::cin >> y;	std::cout << "Enter Pyramid height :=: ";	std::cin >> h;	z = (x * y * h);	return z;}
 
 
 };
@@ -84,8 +84,8 @@ class SSr {
 public:
 	double x, y, z;
 
-	int Squ(int) { system("CLS"); cout << "You have choosen squared ...." << endl; cout << "Enter x :=: ";cin >> x;  z = x*x;return z; }
-	int SqR(int) { system("CLS"); cout << "You have choosen squared ...." << endl; cout << "Enter x :=: ";cin >> x;  z = sqrt(x);return z; }
+	int Squ(int) { system("CLS"); std::cout << "You have choosen squared ...." <<std::endl; std::cout << "Enter x :=: ";std::cin >> x;  z = x*x;return z; }
+	int SqR(int) { system("CLS"); std::cout << "You have choosen squared ...." <<std::endl; std::cout << "Enter x :=: ";std::cin >> x;  z = sqrt(x);return z; }
 
 };
 SSr Sm;
@@ -97,16 +97,16 @@ public:
 
 	int cases(int) {
 	
-		cout << "Choose what you need help with  " << endl;
-		cout << "1. addition			7. Area of rectangle			13. Volume of cube" << endl;
-		cout << "2. subtraction			8. Area of circle			14. Volume of pyramid" << endl;
-		cout << "3. multiplying			9. Perimeter of sqare			15. Volume of rectangle" << endl;
-		cout << "4. dividing			10. Perimeter of triangle		16. Volume of sphere" << endl;
-		cout << "5. Area of sqare		11. Perimeter of circle			17. squared of a number " << endl;
-		cout << "6. Area of triangle		12. Perimeter of rectangle		18.square root  of a number " << endl << endl;
-		cout << "0. for exit \n" << endl;
-		cout << "Enter the number  ";
-		cin >> case_;return case_;
+		std::cout << "Choose what you need help with  " <<std::endl;
+		std::cout << "1. addition			7. Area of rectangle			13. Volume of cube" <<std::endl;
+		std::cout << "2. subtraction			8. Area of circle			14. Volume of pyramid" <<std::endl;
+		std::cout << "3. multiplying			9. Perimeter of sqare			15. Volume of rectangle" <<std::endl;
+		std::cout << "4. dividing			10. Perimeter of triangle		16. Volume of sphere" <<std::endl;
+		std::cout << "5. Area of sqare		11. Perimeter of circle			17. squared of a number " <<std::endl;
+		std::cout << "6. Area of triangle		12. Perimeter of rectangle		18.square root  of a number " <<std::endl <<std::endl;
+		std::cout << "0. for exit \n" <<std::endl;
+		std::cout << "Enter the number  ";
+		std::cin >> case_;return case_;
 	}
 };
 _menu men;
@@ -118,41 +118,41 @@ int main()
 	switch (men.case_)
 	{
 	case 0:
-	case 1:	m.add(m.x, m.y); cout << endl; break;
-	case 2:	m.sub(m.x, m.y); cout << endl; break;
-	case 3:	m.mult(m.x, m.y); cout << endl; break;
-	case 4:	m.div(m.x, m.y); cout << endl; break;
+	case 1:	m.add(m.x, m.y); std::cout <<std::endl; break;
+	case 2:	m.sub(m.x, m.y); std::cout <<std::endl; break;
+	case 3:	m.mult(m.x, m.y); std::cout <<std::endl; break;
+	case 4:	m.div(m.x, m.y); std::cout <<std::endl; break;
 
-	case 5:	Am.AoS(Am.x); cout << endl; break;
-	case 6:	Am.AoT(Am.x, Am.y); cout << endl; break;
-	case 7:	Am.AoR(Am.x, Am.y); cout << endl; break;
-	case 8:	Am.AoC(Am.x); cout << endl; break;
+	case 5:	Am.AoS(Am.x); std::cout <<std::endl; break;
+	case 6:	Am.AoT(Am.x, Am.y); std::cout <<std::endl; break;
+	case 7:	Am.AoR(Am.x, Am.y); std::cout <<std::endl; break;
+	case 8:	Am.AoC(Am.x); std::cout <<std::endl; break;
 
-	case 9:	Pm.PoS(Pm.x); cout << endl; break;
-	case 10: Pm.PoT(Pm.x, Pm.y, Pm.z); cout << endl; break;
-	case 11: Pm.PoC(Pm.r); cout << endl; break;
-	case 12: Pm.PoR(Pm.x, Pm.y); cout << endl; break;
+	case 9:	Pm.PoS(Pm.x); std::cout <<std::endl; break;
+	case 10: Pm.PoT(Pm.x, Pm.y, Pm.z); std::cout <<std::endl; break;
+	case 11: Pm.PoC(Pm.r); std::cout <<std::endl; break;
+	case 12: Pm.PoR(Pm.x, Pm.y); std::cout <<std::endl; break;
 
-	case 13:	Vm.VoC(Vm.x); cout << endl; break;
-	case 14:	Vm.VoP(Vm.x, Vm.y, Vm.h); cout << endl; break;
-	case 15:	Vm.VoR(Vm.x, Vm.y, Vm.z); cout << endl; break;
-	case 16:	Vm.VoS(Vm.x); cout << endl; break;
+	case 13:	Vm.VoC(Vm.x); std::cout <<std::endl; break;
+	case 14:	Vm.VoP(Vm.x, Vm.y, Vm.h); std::cout <<std::endl; break;
+	case 15:	Vm.VoR(Vm.x, Vm.y, Vm.z); std::cout <<std::endl; break;
+	case 16:	Vm.VoS(Vm.x); std::cout <<std::endl; break;
 	
 
-	case 17:	Sm.Squ(Sm.x); cout << endl; break;
-	case 18:	Sm.SqR(Sm.x); cout << endl; break;
+	case 17:	Sm.Squ(Sm.x); std::cout <<std::endl; break;
+	case 18:	Sm.SqR(Sm.x); std::cout <<std::endl; break;
 
 	default:
 		// operator is doesn't match any case constant (+, -, *, /)
-		cout << "Error! operator is not correct " <<endl;
+		std::cout << "Error! operator is not correct " <<std::endl;
 		break;
 	}
 
-	if (men.case_ > 0 && men.case_ < 5) { cout << "z = " << m.z << endl << endl; }
-	else if (men.case_ > 4 && men.case_ < 9) { cout << "z = " << Am.z << " cm^2" << endl << endl; }
-	else if (men.case_ > 8 && men.case_ < 13) { cout << "z = " << Pm.z << " cm" << endl << endl; }
-	else if (men.case_ > 12 && men.case_ < 16) { cout << "z = " << Vm.z << " cm^3" << endl << endl; }
-	else if (men.case_ > 16 && men.case_ < 19) { cout << "z = " << Sm.z  << endl << endl; }
+	if (men.case_ > 0 && men.case_ < 5) { std::cout << "z = " << m.z <<std::endl <<std::endl; }
+	else if (men.case_ > 4 && men.case_ < 9) { std::cout << "z = " << Am.z << " cm^2" <<std::endl <<std::endl; }
+	else if (men.case_ > 8 && men.case_ < 13) { std::cout << "z = " << Pm.z << " cm" <<std::endl <<std::endl; }
+	else if (men.case_ > 12 && men.case_ < 16) { std::cout << "z = " << Vm.z << " cm^3" <<std::endl <<std::endl; }
+	else if (men.case_ > 16 && men.case_ < 19) { std::cout << "z = " << Sm.z  <<std::endl <<std::endl; }
 
 system("pause");
 system("CLS");
